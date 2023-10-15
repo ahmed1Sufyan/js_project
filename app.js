bae9efd8eb0c4e4137a2515d24b2529a639a55b3
= <csc21s135@gmail.com>
2023-10-15 20:43:23 +0500

    first commit

---
+++ app.js
@@ -0,0 +1,65 @@
+// var num1 = +prompt('Enter number 1: ')
+// var num2 = +prompt('Enter number 2: ')
+// var add = num1 + num2
+// var minus = num1 -num2
+// var mul = num1 *num2
+// var div = num1 /num2
+// var modulas = num1 %num2
+// document.write(
+//     "<table border= '10px'>"+
+//     "<tr>"+
+//     "<td>"+"Add"+"</td>"+
+//     "<td>"+"Minus"+"</td>"+
+//     "<td>"+"Mul"+"</td>"+
+//     "<td>"+"Div"+"</td>"+
+//     "<td>"+"Modulas"+"</td>"
+//     +"</tr>"+
+//     "<tr>"+
+//     "<td>"+add+"</td>"+
+//     "<td>"+minus+"</td>"+
+//     "<td>"+mul+"</td>"+
+//     "<td>"+div+"</td>"+
+//     "<td>"+modulas+"</td>"
+//     +"</tr>"
+//     +"</table>"
+// )
+
+ //post increment num++
+ //pre increment ++num
+var add=10+4+4-5+3+5-1+2+5+1+5
+document.write(add)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
